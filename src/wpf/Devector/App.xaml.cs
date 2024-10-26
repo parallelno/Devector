083e3f59f693fb26ae00472fa84d095e5355d3a7
parallelno <mailforfriendparallelno@gmail.com>
2024-10-26 01:24:42 -0700

    integrated wpf project

---
+++ src/wpf/Devector/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+//using dev;
+
+namespace Devector
+{
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ src/wpf/Devector/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using dev;
+
+namespace Devector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AllocConsole();  // This will open a console window
+            
+            HAL obj = new HAL(42, 3.14f);
+
+            obj.field1 += 20;
+
+            // Call the method from the C++/CLI class
+            obj.DisplayData();
+
+            var err = HAL.DisplayData2();
+        }
+    }
+}
